02267ef0a5c08c95e6db2231f5b085ac487bafb2
Shivam <shivamrathi2.sr@gmail.com>
2021-09-30 05:39:15 +0530

    iframe test. added frame.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
   return (
     <Provider store={store}>
       <H1></H1>
+      <iframe
+        title="shivam rathi resume"
+        src="https://drive.google.com/file/d/1ybYBOsl5osc27ZihU4pSLoAMop6uKXSG/preview"
+        width="640"
+        height="480"
+        allow="autoplay"
+      ></iframe>
     </Provider>
   );
 }
